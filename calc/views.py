f1b1e479df29356d1afae157b86a6fd14337404d
ASTER <asterrebeiro69@gmail.com>
2024-04-07 20:51:55 +0530

    fist commit

--- calc/views.py
+++ calc/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html', {'name': 'Alister'})
