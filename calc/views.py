9cdd39f4ca41230378a81c06cc977272badbc948
ASTER <asterrebeiro69@gmail.com>
2024-04-13 11:52:57 +0530

    fist commit

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name': 'Alister'})
+
+def add(request): 
+    result = int(request.POST["num1"]) + int(request.POST["num2"])
+    return render(request, 'result.html', {'result': result})
