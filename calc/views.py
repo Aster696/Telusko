6ab3155b05184a588d2ad60b97d5ac31f7960f5c
Aster696 <aster.r@codezen.in>
2024-04-15 10:06:27 +0530

    fsit commit

--- calc/views.py
+++ calc/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Destination
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name': 'Alister'})
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'Mumbai - 400099'
+    dest1.price = '777'
+    return render(request, 'home.html', {'dest1': dest1})
 
 def add(request): 
     result = int(request.POST["num1"]) + int(request.POST["num2"])
