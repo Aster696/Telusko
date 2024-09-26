2c52ef4a4c13fc77d75275bd14920c2afc6c8466
Aster07102000 <aster.r@codezen.in>
2024-09-26 08:32:04 +0530

    fist commit

--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 def index(request):
-    return render(request, 'travello.html')
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'Mumbai - 400099'
+    dest1.price = '777'
+    dest1.offer = False
+
+    dest2 = Destination()
+    dest2.name = 'Goa'
+    dest2.desc = 'Goa - 400869'
+    dest2.price = '1000'
+    dest2.offer = True
+
+    dest3 = Destination()
+    dest3.name = 'Dheli'
+    dest3.desc = 'Dheli - 603099'
+    dest3.price = '500'
+    dest3.offer = False
+    
+    data = [dest1, dest2, dest3]
+    return render(request, 'travello.html', {"list": data})
