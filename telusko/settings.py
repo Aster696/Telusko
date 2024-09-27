d7b95c985bbc7841dcb54b99a6ba7f8f133aae76
Aster07102000 <aster.r@codezen.in>
2024-09-27 19:51:26 +0530

    rist

--- telusko/settings.py
+++ telusko/settings.py
@@ -132,3 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# for media like image, video, etc
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- travello/views.py
+++ travello/views.py
@@ -3,23 +3,5 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = 'Mumbai - 400099'
-    dest1.price = '777'
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = 'Goa'
-    dest2.desc = 'Goa - 400869'
-    dest2.price = '1000'
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.name = 'Dheli'
-    dest3.desc = 'Dheli - 603099'
-    dest3.price = '500'
-    dest3.offer = False
-    
-    data = [dest1, dest2, dest3]
+    data = Destination.objects.all()
     return render(request, 'travello.html', {"list": data})
